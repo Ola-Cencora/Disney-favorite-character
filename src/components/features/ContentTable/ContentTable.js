510fee6063c1130ebfb14e63940af7bdf158b697
Ola <cencora.aleksandra@gmail.com>
2024-05-22 14:53:43 +0200

    Add basic structure for pagination and content table

--- src/components/features/ContentTable/ContentTable.js
+++ src/components/features/ContentTable/ContentTable.js
@@ -1,7 +1,33 @@
+import { useState } from "react";
+import styles from './ContentTable.module.scss';
+
 const ContentTable = () => {
-    return (
-        <p>table</p>
-    );
+  const [characters, setCharacters] = useState([]);
+
+  return (
+    <table className={styles.table}>
+      <tr>
+        <th>Name</th>
+        <th>Movie</th>
+        <th>Video game</th>
+        <th>Me!</th>
+        <th>Choose me</th>
+      </tr>
+      {characters.map(({ _id, name, films, videoGames, imageUrl }) => (
+        <tr key={_id}>
+          <td>{name}</td>
+          <td>{films}</td>
+          <td>{videoGames}</td>
+          <td>
+            <img src={imageUrl} alt={name} />
+          </td>
+          <td>
+            <input type="checkbox" />
+          </td>
+        </tr>
+      ))}
+    </table>
+  );
 };
 
 export default ContentTable;
--- src/components/features/Pagination/Pagination.js
+++ src/components/features/Pagination/Pagination.js
@@ -1,6 +1,18 @@
+import Button from "../../common/Button/Button";
+import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
+import { MdKeyboardDoubleArrowRight } from "react-icons/md";
+import { MdKeyboardArrowLeft } from "react-icons/md";
+import { MdKeyboardArrowRight } from "react-icons/md";
+
+
 const Pagination = () => {
     return (
-        <p>pagination</p>
+        <section>
+            <Button content={<MdKeyboardDoubleArrowLeft />} />
+            <Button content={<MdKeyboardArrowLeft />} />
+            <Button content={<MdKeyboardArrowRight />} />
+            <Button content={<MdKeyboardDoubleArrowRight />} />
+        </section>
     );
 };
 
