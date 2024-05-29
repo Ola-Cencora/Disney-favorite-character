d68435006c7a565b6ebec3861eda2cebddb1568d
Ola <cencora.aleksandra@gmail.com>
2024-05-29 15:48:48 +0200

    Resolve warnings about table of characters

--- src/components/features/CharactersTable/CharactersTable.js
+++ src/components/features/CharactersTable/CharactersTable.js
@@ -29,32 +29,36 @@ const CharactersTable = ({ currentPage, characters }) => {
 
   return (
     <table className={styles.table}>
-      <tr>
-        <th>Name</th>
-        <th>Movie</th>
-        <th>Video games</th>
-        <th>Me!</th>
-        <th>Choose me</th>
-      </tr>
-      {currentCharacters.map(({ _id, name, films, videoGames, imageUrl }) => (
-        <>
-          {films.length > 0 ? (
-            films.map((film, index) => (
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Movie</th>
+          <th>Video games</th>
+          <th>Me!</th>
+          <th>Choose me</th>
+        </tr>
+      </thead>
+      <tbody>
+        {currentCharacters.map(({ _id, name, films, videoGames, imageUrl }) => {
+          if (films.length > 0) {
+            return films.map((film, index) => (
               <tr key={`${_id}-${index + 1}`}>
                 <td>{name}</td>
                 <td>{film}</td>
                 {characterInfo(videoGames, imageUrl, name)}
               </tr>
-            ))
-          ) : (
-            <tr key={_id}>
-              <td>{name}</td>
-              <td>no info</td>
-              {characterInfo(videoGames, imageUrl, name)}
-            </tr>
-          )}
-        </>
-      ))}
+            ));
+          } else {
+            return (
+              <tr key={_id}>
+                <td>{name}</td>
+                <td>no info</td>
+                {characterInfo(videoGames, imageUrl, name)}
+              </tr>
+            );
+          }
+        })}
+      </tbody>
     </table>
   );
 };
